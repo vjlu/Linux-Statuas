fcdff2c516ce7a6faea7e8eb12f227fd1a926ab6
vjlu <129639234+vjlu@users.noreply.github.com>
2023-04-02 06:22:12 +0300

    Add files via upload

---
+++ Linux-Status/Form1.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(2, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(355, 296)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(314, 7)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(11, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "-"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(330, 7)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "x"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.White
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(343, 262)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "|| Checking ||"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(6, 237)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(132, 16)
+        Me.Label10.TabIndex = 12
+        Me.Label10.Text = "Threads (Seconds) : "
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.Black
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.ForeColor = System.Drawing.Color.White
+        Me.TextBox3.Location = New System.Drawing.Point(141, 237)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(190, 21)
+        Me.TextBox3.TabIndex = 11
+        Me.TextBox3.Text = "15"
+        '
+        'Label8
+        '
+        Me.Label8.ForeColor = System.Drawing.Color.Aqua
+        Me.Label8.Location = New System.Drawing.Point(54, 213)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(265, 16)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "0"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.ForeColor = System.Drawing.Color.Aqua
+        Me.Label9.Location = New System.Drawing.Point(6, 213)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(45, 16)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Error : "
+        '
+        'Label7
+        '
+        Me.Label7.ForeColor = System.Drawing.Color.Red
+        Me.Label7.Location = New System.Drawing.Point(66, 196)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(265, 16)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "0"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label6
+        '
+        Me.Label6.ForeColor = System.Drawing.Color.Lime
+        Me.Label6.Location = New System.Drawing.Point(71, 180)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(265, 16)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "0"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(6, 196)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(59, 16)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Closed : "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.Lime
+        Me.Label4.Location = New System.Drawing.Point(6, 180)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 16)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Opened : "
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(6, 147)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(331, 30)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(6, 121)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(137, 20)
+        Me.CheckBox2.TabIndex = 3
+        Me.CheckBox2.Text = "Send to Telegram"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 95)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(114, 20)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "Infinite Checks"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Black
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.Color.White
+        Me.TextBox2.Location = New System.Drawing.Point(6, 61)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(331, 28)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "Port"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(6, 27)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(331, 28)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "The Public IP"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(348, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Open Ports Checker By Vjlu"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(359, 300)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Ports-Status By Vjlu"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Linux-Status/Form1.vb
@@ -0,0 +1,177 @@
+﻿Imports System.Net
+Imports System.Text, System.Text.RegularExpressions, System.Security, System.IO, System.Threading
+
+Public Class Form1
+    Dim TeleInfo As String
+    Dim bottoken As String
+    Dim acid As String
+    Dim Pos As Point
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        End
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+    Public Sub send_message(msg As String)
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+        Try
+            Dim a As New WebClient()
+            a.DownloadString("https://api.telegram.org/bot" + bottoken + "/sendMessage?chat_id=" + acid + "&text=" + msg)
+        Catch ex As Exception
+        End Try
+    End Sub
+    Dim x As Integer = 0
+    Public Function check1()
+        If CheckBox1.Checked = True Then
+            Do While x = 0
+                Try
+                    Net.ServicePointManager.CheckCertificateRevocationList = False
+                    Net.ServicePointManager.DefaultConnectionLimit = 300
+                    Net.ServicePointManager.UseNagleAlgorithm = False
+                    Net.ServicePointManager.Expect100Continue = False
+                    Net.ServicePointManager.SecurityProtocol = 3072
+                    Dim Encoding As New Text.UTF8Encoding
+                    Dim Bytes As Byte() = Encoding.GetBytes("remoteAddress=" + TextBox1.Text + "&portNumber=" + TextBox2.Text + "")
+                    Dim Request As Net.HttpWebRequest = DirectCast(Net.WebRequest.Create("https://ports.yougetsignal.com/check-port.php"), Net.HttpWebRequest)
+                    With Request
+                        .Method = "POST"
+                        .KeepAlive = False
+                        .UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
+                        .ContentType = "application/x-www-form-urlencoded; charset=UTF-8"
+                        .Accept = "text/javascript, text/html, application/xml, text/xml, */*"
+                        .Headers.Add("Accept-Language: en-US,en;q=0.9,ar;q=0.8")
+                        .AutomaticDecompression = Net.DecompressionMethods.Deflate Or Net.DecompressionMethods.GZip
+                        .ContentLength = Bytes.Length
+                    End With
+                    Dim Stream As IO.Stream = Request.GetRequestStream()
+                    Stream.Write(Bytes, 0, Bytes.Length)
+                    Stream.Dispose()
+                    Stream.Close()
+                    Dim Reader As New IO.StreamReader(DirectCast(Request.GetResponse(), Net.HttpWebResponse).GetResponseStream())
+                    Dim Text As String = Reader.ReadToEnd
+                    Reader.Dispose()
+                    Reader.Close()
+                    If Text.Contains("<p><img src=""/img/flag_green.gif"" alt=""Open"" style=""height: 1em; width: 1em;"" /> Port <a href=""https://en.wikipedia.org/wiki/Port_" + TextBox2.Text + """ target=""_blank"" />" + TextBox2.Text + "</a> is open on " + TextBox1.Text + ".</p>") Then
+                        Label6.Text += 1
+                    Else
+                        Label7.Text += 1
+                        If CheckBox2.Checked = True Then
+                            send_message("The Ports Tool Checker Couldn't reach Port " + TextBox2.Text + " on " + TextBox1.Text + " Right now , Please investigate immediately.")
+                        End If
+                    End If
+                    Thread.Sleep(TextBox3.Text * 1000)
+                Catch ex As WebException
+                    Label8.Text += 1
+                    send_message("Exception: There is Unknown Error with the connection , please fix")
+                End Try
+            Loop
+        Else
+            Try
+                Net.ServicePointManager.CheckCertificateRevocationList = False
+                Net.ServicePointManager.DefaultConnectionLimit = 300
+                Net.ServicePointManager.UseNagleAlgorithm = False
+                Net.ServicePointManager.Expect100Continue = False
+                Net.ServicePointManager.SecurityProtocol = 3072
+                Dim Encoding As New Text.UTF8Encoding
+                Dim Bytes As Byte() = Encoding.GetBytes("remoteAddress=" + TextBox1.Text + "&portNumber=" + TextBox2.Text + "")
+                Dim Request As Net.HttpWebRequest = DirectCast(Net.WebRequest.Create("https://ports.yougetsignal.com/check-port.php"), Net.HttpWebRequest)
+                With Request
+                    .Method = "POST"
+                    .KeepAlive = False
+                    .UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
+                    .ContentType = "application/x-www-form-urlencoded; charset=UTF-8"
+                    .Accept = "text/javascript, text/html, application/xml, text/xml, */*"
+                    .Headers.Add("Accept-Language: en-US,en;q=0.9,ar;q=0.8")
+                    .AutomaticDecompression = Net.DecompressionMethods.Deflate Or Net.DecompressionMethods.GZip
+                    .ContentLength = Bytes.Length
+                End With
+                Dim Stream As IO.Stream = Request.GetRequestStream()
+                Stream.Write(Bytes, 0, Bytes.Length)
+                Stream.Dispose()
+                Stream.Close()
+                Dim Reader As New IO.StreamReader(DirectCast(Request.GetResponse(), Net.HttpWebResponse).GetResponseStream())
+                Dim Text As String = Reader.ReadToEnd
+                Reader.Dispose()
+                Reader.Close()
+                If Text.Contains("<p><img src=""/img/flag_green.gif"" alt=""Open"" style=""height: 1em; width: 1em;"" /> Port <a href=""https://en.wikipedia.org/wiki/Port_" + TextBox2.Text + """ target=""_blank"" />" + TextBox2.Text + "</a> is open on " + TextBox1.Text + ".</p>") Then
+                    Label6.Text += 1
+                Else
+                    Label7.Text += 1
+                    If CheckBox2.Checked = True Then
+                        send_message("The Ports Tool Checker Couldn't reach Port " + TextBox2.Text + " on " + TextBox1.Text + " Right now , Please investigate immediately.")
+                    End If
+                End If
+            Catch ex As WebException
+                Label8.Text += 1
+                send_message("Exception: There is Unknown Error with the connection , please fix")
+            End Try
+        End If
+
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+        Try
+            TeleInfo = File.ReadAllText("Telegram.txt")
+            bottoken = Regex.Match(TeleInfo, """BotToken=(.*?)""").Groups(1).Value
+            acid = Regex.Match(TeleInfo, """ID=(.*?)""").Groups(1).Value
+        Catch ex As Exception
+            File.WriteAllText("Telegram.txt", """BotToken=Your Bot Token Here""" + vbCrLf + """ID=YourID""")
+        End Try
+        Dim b As String = MsgBox("Do you want Me" + vbCrLf + "To grab your Public IP?", MessageBoxButtons.YesNo)
+        If b = 6 Then
+            grabIB()
+        End If
+    End Sub
+    Function grabIB()
+        Try
+            Net.ServicePointManager.CheckCertificateRevocationList = False
+            Net.ServicePointManager.DefaultConnectionLimit = 300
+            Net.ServicePointManager.UseNagleAlgorithm = False
+            Net.ServicePointManager.Expect100Continue = False
+            Net.ServicePointManager.SecurityProtocol = 3072
+            Dim Request As Net.HttpWebRequest = DirectCast(Net.WebRequest.Create("https://ip.me/"), Net.HttpWebRequest)
+            With Request
+                .Method = "GET"
+                .Proxy = Nothing
+                .Host = "ip.me"
+                .KeepAlive = False
+                .UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
+                .Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+                .Headers.Add("Accept-Language: en-US,en;q=0.9,ar;q=0.8")
+                .AutomaticDecompression = Net.DecompressionMethods.Deflate Or Net.DecompressionMethods.GZip
+            End With
+            Dim Response As Net.HttpWebResponse = DirectCast(Request.GetResponse, Net.HttpWebResponse)
+            Dim reader As New IO.StreamReader(Response.GetResponseStream)
+            Dim text As String = reader.ReadToEnd
+            reader.Dispose()
+            reader.Close()
+            Response.Dispose()
+            Response.Close()
+            TextBox1.Text = Regex.Match(text, "<input type=""text"" name=""ip"" value=""(.*?)"" class=""form-control"" id=""ip-lookup""").Groups(1).Value
+        Catch ex As WebException
+            MsgBox("Sorry, Something went wrong while Grabbing Your IP", MessageBoxIcon.Error)
+        End Try
+    End Function
+    Dim t As Thread = New Thread(AddressOf check1)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If CheckBox1.Checked = True Then
+            CheckBox1.Enabled = False
+            t.Start()
+            t.IsBackground = True
+            Button1.Text = "Stop"
+        ElseIf Button1.Text = "Stop" Then
+            t.Abort()
+            Button1.Text = "Start"
+        Else
+            check1()
+        End If
+    End Sub
+
+    Private Sub Label1_MouseMove(sender As Object, e As MouseEventArgs) Handles Label1.MouseMove
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Me.Location += Control.MousePosition - Pos
+        End If
+        Pos = Control.MousePosition
+    End Sub
+End Class
